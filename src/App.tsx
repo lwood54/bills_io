708fbbce87a4afc37c2eefd20e84da64650e5c6e
lwood54 <lwood3499@gmail.com>
2021-12-24 23:03:55 -0600

    initial setup: vite-react-ts-tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Nav from "./components/Nav/nav";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <div className="bg-emerald-400">test</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/nav.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const Nav: React.FC = () => {
+  return (
+    <div>
+      <h1 className="bg-black w-64">This is the nav bar.</h1>
+    </div>
+  );
+};
+
+export default Nav;
