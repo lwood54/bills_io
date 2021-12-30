0300329cccbb27af683a5d478c3c2f9fbf62bb9f
lwood54 <lwood3499@gmail.com>
2021-12-29 20:01:15 -0600

    update to use rhf, bring in immer and ts-pattern

--- src/components/Base/FormInput/form-input.tsx
+++ src/components/Base/FormInput/form-input.tsx
@@ -1,42 +1,47 @@
 import * as React from "react";
 
-interface FormInputProps {
+interface InputProps {
   customClass?: string;
   containerClass?: string;
-  id: string;
-  isStacked?: boolean;
-  label?: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>, id: string) => void;
+  defaultValue?: string | number;
+  error?: string;
+  id?: string;
+  label: string;
   placeholder?: string;
-  scale?: "s" | "m" | "l" | "xl";
-  type: string;
-  value: string | number;
 }
-const FormInput: React.FC<FormInputProps> = ({
-  containerClass = "",
-  customClass = "",
-  id,
-  isStacked = true,
-  label,
-  onChange,
-  placeholder,
-  scale = "m",
-  type,
-  value,
-}) => {
-  return (
-    <div className={`${containerClass} w-96`}>
-      <label htmlFor={id}>{label}</label>
-      <input
-        onChange={(e) => onChange(e, id)}
-        type={type}
-        id={id}
-        placeholder={placeholder}
-        className={`appearance-none w-full rounded-t py-3 px-4 leading-tight border border-b-4 border-b-teal-400 py-3 px-4 focus:outline-none focus:bg-teal-50 focus:border-teal-400 ${customClass}`}
-        value={value}
-      />
-    </div>
-  );
-};
+const FormInput: React.FC<InputProps> = React.forwardRef(
+  (
+    {
+      containerClass = "",
+      customClass = "",
+      defaultValue,
+      error,
+      id,
+      label,
+      placeholder,
+      ...rest
+    },
+    ref
+  ) => {
+    return (
+      <div className={`${containerClass} w-96`}>
+        <label htmlFor={id}>{label}</label>
+        <input
+          ref={ref as React.RefObject<HTMLInputElement>}
+          defaultValue={defaultValue}
+          id={id}
+          placeholder={placeholder}
+          className={`${
+            error
+              ? "border border-red-600 rounded"
+              : "border border-b-4 border-b-teal-400"
+          } appearance-none w-full rounded-t py-3 px-4 leading-tight py-3 px-4 focus:outline-none focus:bg-teal-50 focus:border-teal-400 ${customClass}`}
+          {...rest}
+        />
+        {error && <p className="text-red-600 text-xs">{error}</p>}
+      </div>
+    );
+  }
+);
 
 export default FormInput;
